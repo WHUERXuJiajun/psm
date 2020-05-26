9c86d7964f2fc9b319cbb34ad664f4588380c189
theory <827041735@qq.com>
2020-05-26 08:01:24 +0800

    feat：添加了注册功能

---
+++ src/main/resources/static/json/user/reg.js
@@ -0,0 +1,52 @@
+$(function () {
+    $("#register").click(function () {
+        let username = $("#phone").val();
+        let password = $("#pwd").val();
+        let rePwd = $("#rePwd").val();
+        if (!username) {
+            alert("电话必填!");
+            $("#username").focus();//获取焦点
+            return;
+        }
+        if (!password) {
+            alert("密码必填!");
+            $("#password").focus();//获取焦点
+            return;
+        }
+        //验证手机号码格式
+        if(!(/^1[3456789]\d{9}$/.test(username))){
+            alert("手机号码有误，请重填");
+            return false;
+        }
+        if(password.length < 6){
+            alert("密码不符合要求，请重新填写");
+            return false;
+        }
+
+        //验证重复密码是否正确
+        if(password !== rePwd){
+            alert("密码前后填写不一致!");
+            return false;
+        }
+
+
+        $.ajax({
+            type: "POST",
+            url: "/user/register",//请求程序页面
+            async: false,//当有返回值以后才会进行后面的js程序。
+            data: {"phone": username, "pwd": password},//请求需要发送的处理数据
+            success: function (data) {
+                if (data == true) {
+                    alert("注册成功！");
+                    window.location.href = 'login.html';
+                } else {
+                    alert("注册失败，电话号码可能已被注册，请重新尝试");
+                }
+            }
+        });
+        return false;
+    });
+});
+
+
+
