3b335df6a63da6b68627fec0a197d48224c976c2
yurivkunn <2928630897@qq.com>
2020-05-29 15:50:57 +0800

    test:测试CommentServiceImpl

---
+++ src/test/java/whu/web/psm/service/impl/CommentServiceImplTest.java
@@ -0,0 +1,32 @@
+package whu.web.psm.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import whu.web.psm.pojo.Comment;
+import whu.web.psm.util.UUIDUtil;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentServiceImplTest {
+
+    @Autowired
+    private CommentServiceImpl commentService;
+
+    @Test
+    public void getCommentsByMid() {
+        assertNotNull(commentService.getCommentsByMid(1));
+    }
+
+    @Test
+    public void postComment() {
+        Comment comment = new Comment();
+        comment.setMid(1);
+        comment.setContent("这是测试评论");
+        comment.setFrom_user("test");
+        commentService.postComment(comment);
+    }
+}
