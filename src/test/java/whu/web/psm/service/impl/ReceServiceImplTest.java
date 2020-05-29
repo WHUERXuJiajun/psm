49b01769e8c3a81bf20a5ba54abb5da8e5e8b4e3
yurivkunn <2928630897@qq.com>
2020-05-29 11:43:10 +0800

    test:测试ReceServiceImpl

---
+++ src/test/java/whu/web/psm/service/impl/ReceServiceImplTest.java
@@ -0,0 +1,50 @@
+package whu.web.psm.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import whu.web.psm.pojo.ReceKey;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReceServiceImplTest {
+
+    @Autowired
+    private ReceServiceImpl receService;
+
+    String phone = "15387315836";
+    int mid = 4;
+
+    @Test
+    public void insertRece() {
+        boolean res;
+        ReceKey receKey = new ReceKey();
+        receKey.setPhone(phone);
+        receKey.setMid(mid);
+        res = receService.insertRece(receKey);
+        assertTrue(res);
+        //任务id不存在时
+        receKey.setMid(10000);
+        res = receService.insertRece(receKey);
+        assertFalse(res);
+    }
+
+    @Test
+    public void getMissionsByPhone() {
+        assertNotNull(receService.getMissionsByPhone(phone));
+    }
+
+    @Test
+    public void cancelMission() {
+        boolean res;
+        ReceKey receKey = new ReceKey();
+        receKey.setPhone(phone);
+        receKey.setMid(8);
+        res = receService.cancelMission(receKey);
+        assertTrue(res);
+    }
+}
