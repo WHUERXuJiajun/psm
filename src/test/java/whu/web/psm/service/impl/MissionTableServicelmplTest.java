42d862c3d8e9872c945d62c1e46fb7bcaef262d5
yurivkunn <2928630897@qq.com>
2020-05-30 16:59:56 +0800

    test:完善MissionTableServiceImpl测试

--- src/test/java/whu/web/psm/service/impl/MissionTableServicelmplTest.java
+++ src/test/java/whu/web/psm/service/impl/MissionTableServicelmplTest.java
@@ -14,6 +14,8 @@ public class MissionTableServicelmplTest {
     @Autowired
     private MissionTableServicelmpl missionTableServicelmpl;
 
+    String phone = "17371447819";
+
     @Test
     public void getMissions_all() {
         //size=0
@@ -23,7 +25,7 @@ public class MissionTableServicelmplTest {
 
     @Test
     public void getDetails() {
-        assertNotNull(missionTableServicelmpl.getDetails(1));
+        assertNotNull(missionTableServicelmpl.getDetails(4));
     }
 
     @Test
@@ -32,4 +34,33 @@ public class MissionTableServicelmplTest {
         assertNotNull(missionTableServicelmpl.selectMissionByLabel("宠物", 1, 1));
     }
 
+    @Test
+    public void mission_cancel() {
+        boolean res;
+        res = missionTableServicelmpl.mission_cancel(1);
+        assertTrue(res);
+    }
+
+    @Test
+    public void mission_update() {
+        boolean res;
+        res = missionTableServicelmpl.mission_update(2, "testTitle", "MyTestUpdate",
+                1.1, "label1", "label2", "label3", phone);
+        assertTrue(res);
+    }
+
+    @Test
+    public void selectMissionByTime() {
+        assertNotNull(missionTableServicelmpl.selectMissionByTime(1,1));
+    }
+
+    @Test
+    public void selectMissionByMoney() {
+        assertNotNull(missionTableServicelmpl.selectMissionByMoney(1,1));
+    }
+
+    @Test
+    public void selectMissionByDdl() {
+        assertNotNull(missionTableServicelmpl.selectMissionByDdl(1,1));
+    }
 }
